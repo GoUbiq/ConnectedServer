3cf8509e36777d3fc08f2da7609cd9d3b1ce1d7f
Sumit Pasupalak <pasupalaks@gmail.com>
2013-12-04 14:55:33 -0500

    Cookie

--- server.py
+++ server.py
@@ -1,1 +1,79 @@
-import util
+import sys
+from pymongo import MongoClient
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+mongoClient = MongoClient()
+connected_db = mongoClient['connected_db']
+
+devices = connected_db.devices
+connected = connected_db.connected
+
+
+class ConnectedServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_GET(request):
+        print 'Got Request' + request.path
+        if request.path  == "/connectPhone":
+           request.send_response(200)
+           request.send_header("Content-type", "text/html")
+           request.end_headers()
+           request.wfile.write("<html><head><title>You requested to connectPhone.</title></head>")
+           request.wfile.write("<p>You accessed path: %s</p>" % request.path)
+           request.wfile.write("</body></html>")
+
+        elif request.path == "/connectTV":
+            print "connectTV"
+            post = {'user': 'sumit', 'device':'TV', 'request': 'connectTV'};
+            connected.insert(post)
+
+
+        elif request.path == "/connectLaptop":
+            print "connectLaptop"
+            post = {'user': 'sumit', 'device':'Laptop', 'request': 'connectLaptop'};
+            connected.insert(post)
+
+        elif request.path == "/registerTV":
+            print "registerTV"
+            post = {'user': 'sumit', 'device':'TV', 'request': 'registerTV'};
+
+        elif request.path == "/getConnectedDevices":
+            print "connectedDevices"
+            query = {'user': 'sumit'}
+            connected_devices = connected.find(query)
+            for d in connected_devices:
+                print d
+
+        elif request.path == "/getDevices":
+            print "getDevices"
+
+
+        else:
+            print "Unknown Path"
+
+    def do_POST(request):
+        print request.path
+
+
+def run():
+    print "Run called"
+    HandlerClass = ConnectedServerHandler
+    ServerClass  = BaseHTTPServer.HTTPServer
+    Protocol     = "HTTP/1.0"
+
+    if sys.argv[1:]:
+        port = int(sys.argv[1])
+    else:
+        port = 8000
+    server_address = ('127.0.0.1', port)
+
+    HandlerClass.protocol_version = Protocol
+    httpd = ServerClass(server_address, HandlerClass)
+
+    sa = httpd.socket.getsockname()
+    print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    print " Main Called"
+    run()
